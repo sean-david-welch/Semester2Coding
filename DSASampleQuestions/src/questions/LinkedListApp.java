d3dc22f3a7562ac68dfaa3795c7efd51e2f31552
seanwelch <seanwelch51199@icloud.com>
2024-07-21 18:00:51 +0100

    oops

--- DSASampleQuestions/src/questions/LinkedListApp.java
+++ DSASampleQuestions/src/questions/LinkedListApp.java
@@ -6,26 +6,26 @@ public class LinkedListApp {
 
         System.out.println("######## Created LList #########");
         System.out.println("Is list empty? " + list.isEmpty());
-        System.out.println("LL size: " + list.size()); // 0
+        System.out.println("LL size: " + list.size());
 
         System.out.println("##### Add first subject #####");
         Subject ds = new Subject("DS", 83.7);
         list.add(ds);
-        System.out.println("Is LL empty: " + list.isEmpty()); // false
-        System.out.println("LL size: " + list.size()); // 1
-        System.out.println("LL: " + list.toString());
+        System.out.println("Is LL empty: " + list.isEmpty());
+        System.out.println("LL size: " + list.size());
+        System.out.println("LL: " + list);
 
         System.out.println("##### Add second subject #####");
         Subject oop = new Subject("OOP", 63.5);
         list.add(oop);
-        System.out.println("Is LL empty: " + list.isEmpty()); // false
-        System.out.println("LL size: " + list.size()); // 1
-        System.out.println("LL: " + list.toString());
+        System.out.println("Is LL empty: " + list.isEmpty());
+        System.out.println("LL size: " + list.size());
+        System.out.println("LL: " + list);
 
         System.out.println("##### Remove subject #####");
         System.out.println("Remove DS: " + list.remove("DS"));
-        System.out.println("Is LL empty: " + list.isEmpty()); // false
-        System.out.println("LL size: " + list.size()); // 1
-        System.out.println("LL: " + list.toString());
+        System.out.println("Is LL empty: " + list.isEmpty());
+        System.out.println("LL size: " + list.size());
+        System.out.println("LL: " + list);
     }
 }
