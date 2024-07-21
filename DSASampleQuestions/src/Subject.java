381132487eef726f63432092acbcab570d5d14a0
seanwelch <seanwelch51199@icloud.com>
2024-07-21 12:19:55 +0100

    subject class

--- DSASampleQuestions/src/Subject.java
+++ DSASampleQuestions/src/Subject.java
@@ -1,5 +1,41 @@
 public class Subject {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // Properties
+    private String module;
+    private double grade;
+
+    // Constructors
+    public Subject() {};
+
+    public Subject(String module, double grade) {
+        this.module = module;
+        this.grade = grade;
     }
+
+    public Subject(String module) {
+        this.module = module;
+    }
+
+    // Getters
+    public String getModule() {
+        return module;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    // Setters
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    // Methods
+    @Override
+    public String toString() {
+        return String.format("Subject {module=%s, grade=%s}", module, grade);
+    };
 }
