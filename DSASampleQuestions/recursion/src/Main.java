e490409048eb0a69339f229b6fc872ccc2c1ca3f
seanwelch <seanwelch51199@icloud.com>
2024-07-22 20:03:04 +0100

    exponent

--- DSASampleQuestions/recursion/src/Main.java
+++ DSASampleQuestions/recursion/src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(factorial(7));
+        System.out.println(power(2, 8));
     }
 
     private static int factorial(int num) {
         if (num < 1) return 1;
         return num * factorial(num - 1);
+    }
+
+    private static int power(int base, int exponent) {
+        if (exponent < 1) return 1;
+        return base * power(base, exponent - 1);
     }
 }
