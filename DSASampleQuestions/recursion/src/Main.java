9f6338dc3fd3022842972851374179cc0dcd3c1a
seanwelch <seanwelch51199@icloud.com>
2024-07-22 19:59:52 +0100

    recursion pal

---
+++ DSASampleQuestions/recursion/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(factorial(7));
+    }
+
+    private static int factorial(int num) {
+        if (num < 1) return 1;
+        return num * factorial(num - 1);
+    }
+}
