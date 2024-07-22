f088ebb9e674b2f058125c5e81f0a2f38e298295
seanwelch <seanwelch51199@icloud.com>
2024-07-22 21:05:44 +0100

    linked list buddy

--- DSASampleQuestions/linkedList/src/questions/LinkedList.java
+++ DSASampleQuestions/linkedList/src/questions/LinkedList.java
@@ -5,14 +5,14 @@ public class LinkedList<T> {
     public Node<T> first;
     public Node<T> last;
 
+    public LinkedList() {}
+
     public boolean isEmpty() {
         return first == null;
     }
 
     public int size() {
-        if (isEmpty()) {
-            return 0;
-        }
+        if (isEmpty()) return 0;
 
         int count = 0;
         Node<T> current = first;
@@ -24,22 +24,18 @@ public class LinkedList<T> {
         return count;
     }
 
-
-    public double getAverage() {
-        if (isEmpty()) {
-            return 0;
-        }
+    public double average() {
+        if (isEmpty()) return 0;
 
         int count = 0;
         double sum = 0;
         Node<T> current = first;
 
         while (current != null) {
-            count ++;
+            count++;
             sum += current.subject.getGrade();
             current = current.next;
         }
-
         return count == 0 ? 0 : sum / count;
     }
 
@@ -54,16 +50,14 @@ public class LinkedList<T> {
         last = current;
     }
 
-    public boolean remove(String subject) {
-        if (isEmpty()) {
-            return false;
-        }
+    public boolean remove(String subjectName) {
+        if (isEmpty()) return false;
 
         Node<T> current = first;
-        Node<T> previous = null;
+        Node<T> prev = null;
 
-        while (current != null && !current.subject.getModule().equalsIgnoreCase(subject)) {
-            previous = current;
+        while (current != null && current.subject.getModule().equalsIgnoreCase(subjectName)) {
+            prev = current;
             current = current.next;
         }
 
@@ -71,34 +65,32 @@ public class LinkedList<T> {
             return false;
         }
 
-        if (previous == null) {
+        if (prev == null) {
             first = first.next;
-            if (first == null) {
-                last = null;
-            }
+            if (first == null) last = null;
         } else {
-            previous.next = current.next;
+            prev.next = current.next;
             if (current.next == null) {
-                last = previous;
+                last = prev;
             }
         }
 
         return true;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder string = new StringBuilder("List: \n");
-        Node<T> current = first;
-
-        if (isEmpty()) {
-            return "The list is Empty";
-        } else {
-            for (int i = 1; i <= size(); i++) {
-                string.append(String.format("%s) %s\n", i, current.toString()));
-                current = current.next;
-            }
-            return string.toString();
-        }
-    }
+//    @Override
+//    public String toString() {
+//        StringBuilder string = new StringBuilder("List: \n");
+//        Node<T> current = first;
+//
+//        if (isEmpty()) {
+//            return "The list is Empty";
+//        } else {
+//            for (int i = 1; i <= size(); i++) {
+//                string.append(String.format("%s) %s\n", i, current.toString()));
+//                current = current.next;
+//            }
+//            return string.toString();
+//        }
+//    }
 }
