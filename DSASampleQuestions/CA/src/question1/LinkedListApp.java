1d21984a870ac3cfe643a1b8c60ad9152094e384
seanwelch <seanwelch51199@icloud.com>
2024-07-25 20:05:20 +0100

    projects

--- DSASampleQuestions/CA/src/question1/LinkedListApp.java
+++ DSASampleQuestions/CA/src/question1/LinkedListApp.java
@@ -1,5 +1,12 @@
 package question1;
 
 public class LinkedListApp {
-    
+    public static void main(String[] args) {
+        Project project1 = new Project("PTDUH1", "Elmer Fud", "Port Tunnel", 1123, "Well off track");
+        Project project2 = new Project("PTDUGB2", "Jane Doe", "Airport", 9988, "Steaming Along!");
+        Project project3 = new Project("PTDFB3", "Damo Delaney", "Sherrif Street", 6543, "The GOAT of them all");
+        Project project4 = new Project("PTDFDV4", "Ivor O'Mahoney", "Private School", 7420, "Bosh");
+        Project project5 = new Project("PTDJA%", "Damo Delaney", "Sports Centre", 6543, "Run of the mill");
+
+    }
 }
