26a7db9ff6bdf321a8f0742b0b7f4f9fd9e4850c
seanwelch <seanwelch51199@icloud.com>
2024-07-25 19:34:14 +0100

    add remove method

--- DSASampleQuestions/CA/src/question1/LinkedList.java
+++ DSASampleQuestions/CA/src/question1/LinkedList.java
@@ -25,4 +25,25 @@ public class LinkedList<T> {
         last.next = current;
         last = current;
     }
+
+    public boolean remove(String projectCode) {
+        if (isEmpty()) return false;
+
+        Node<T> current = first;
+        Node<T> previous = null;
+
+        while (current != null && current.project.getProjectCode().equalsIgnoreCase(projectCode)) {
+            previous = current;
+            current = current.next;
+        }
+
+        if (current == null) return false;
+
+        if (previous != null) {
+            previous.next = current.next;
+        } else {
+            first = current.next;
+        }
+        return true;
+    }
 }
