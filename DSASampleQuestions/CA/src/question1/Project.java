da2830524d547e17ec707f857e22a52b41064bd5
seanwelch <seanwelch51199@icloud.com>
2024-07-25 19:06:32 +0100

    initial properties

---
+++ DSASampleQuestions/CA/src/question1/Project.java
@@ -0,0 +1,9 @@
+package question1;
+
+public class Project {
+    public String projectCode;
+    public String managerName;
+    public String projectDescription;
+    public int projectDuration;
+    public String projectStatus;
+}
