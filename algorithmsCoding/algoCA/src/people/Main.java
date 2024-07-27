15e35ba1568d2c7f4b88e1139b736184748f5dc3
seanwelch <seanwelch51199@icloud.com>
2024-07-27 20:06:29 +0100

    main method for bubble sort

--- algorithmsCoding/algoCA/src/people/Main.java
+++ algorithmsCoding/algoCA/src/people/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args) {
         try {
             PeopleReader peopleReader = new PeopleReader("./resources/people.csv");
+            People[] people = peopleReader.readPeople();
 
-            PeopleSorter peopleSorter = new PeopleSorter();
+            PeopleSorter peopleSorter = new PeopleSorter(people);
+
+            peopleSorter.BubbleSort();
         } catch (Exception e) {
             System.err.println("Failed to read people: " + e.getMessage());
         }
