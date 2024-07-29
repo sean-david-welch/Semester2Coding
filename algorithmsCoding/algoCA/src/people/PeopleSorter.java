1f517bfc41f37b1e9e4e9876c33949b61b93f19d
seanwelch <seanwelch51199@icloud.com>
2024-07-29 17:45:16 +0100

    quick sort algorithm

--- algorithmsCoding/algoCA/src/people/PeopleSorter.java
+++ algorithmsCoding/algoCA/src/people/PeopleSorter.java
@@ -39,7 +39,33 @@ public class PeopleSorter {
         }
     }
 
-    public static void QuickSort() {}
+    public void QuickSort(int low, int high) {
+        if (low < high) {
+            int partition = partition(low, high);
+
+            QuickSort(low, partition - 1);
+            QuickSort(partition + 1, high);
+        }
+    }
+
+    private int partition(int low, int high) {
+        People pivot = people[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (people[j].compareTo(pivot) <= 0) {
+                i++;
+                People temp = people[i];
+                people[i] = people[j];
+                people[j] = temp;
+            }
+        }
+
+        People temp = people[i + 1];
+        people[i + 1] = people[high];
+        people[high] = temp;
+        return i + 1;
+    }
 
     public static void BinarySearch() {}
 }
