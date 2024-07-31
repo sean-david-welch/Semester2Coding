394e312217719a1d6e331b248faca777679ea353
seanwelch <seanwelch51199@icloud.com>
2024-07-31 18:24:45 +0100

    got binary search working

--- algorithmsCoding/algoCA/src/main/java/org/example/main/BinarySearch.java
+++ algorithmsCoding/algoCA/src/main/java/org/example/main/BinarySearch.java
@@ -1,13 +1,34 @@
 package org.example.main;
 
 import org.example.people.People;
+import org.example.people.PeopleReader;
 
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class BinarySearch {
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        try {
+            PeopleReader peopleReader = new PeopleReader("resources/people.csv");
+            People[] people = peopleReader.readPeople();
+
+            BinarySearch binarySearch = new BinarySearch(people);
+
+            String columnToSearch = "name";
+            String targetValue = "Joe";
+
+            int result = binarySearch.binarySearch(columnToSearch, targetValue);
+
+            if (result != -1) {
+                System.out.println(targetValue + " was found in the " + columnToSearch + " list");
+                System.out.println("Record details: " + people[result]);
+            } else {
+                System.out.println(targetValue + " was not found in the " + columnToSearch + " list!");
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to read people: " + e.getMessage());
+        }
     }
 
     private final People[] people;
