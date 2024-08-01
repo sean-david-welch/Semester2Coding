58bfb9f25bc62fd4ca1cc0b34aac0db20cb239cd
seanwelch <seanwelch51199@icloud.com>
2024-08-01 17:55:31 +0100

    comments for binary search

--- algorithmsCoding/algoCA/src/main/java/org/example/main/BinarySearch.java
+++ algorithmsCoding/algoCA/src/main/java/org/example/main/BinarySearch.java
@@ -13,44 +13,64 @@ public class BinarySearch<T> {
     // generic property
     private final T[] data;
 
+    // constructor with generic
     public BinarySearch(T[] data) {
         this.data = data;
     }
 
+    // binary search method
+    // generic type U extends the comparable key which is taken from the data
+    // we pass a generic callback function with T, U in order to implement our comparison on people
+    // the target is of type U
     public <U extends Comparable<U>> int binarySearch(Function<T, U> func, U target) {
+        // init comparator with callback function and sort array based on comparator
         Comparator<T> comparator = Comparator.comparing(func);
         Arrays.sort(data, comparator);
 
+        // init low and high indices as start and end of array - two pointer approach
         int low = 0;
         int high = data.length - 1;
 
+        // loop through until pointers meet
         while (low <= high) {
+            // get mid point
             int mid = (low + high) / 2;
+            // extract generic data from the index of midpoint
             T midVal = data[mid];
+            // apply comparison of midval to target through callback function
             int cmp = func.apply(midVal).compareTo(target);
 
+            // if difference less than 0 were too low
             if (cmp < 0) {
                 low = mid + 1;
+            // if greater than zero were too high
             } else if (cmp > 0) {
                 high = mid - 1;
             } else {
+            // goldilocks zone were just right
                 return mid;
             }
         }
+        // target not found
         return -1;
     }
 
     public static void main(String[] args) {
         // Scanner could be used to take user input of course
         try {
+            // init people reader and read people from csv into memory
             PeopleReader peopleReader = new PeopleReader("resources/people.csv");
             People[] people = peopleReader.readPeople();
 
+            // init binary search
             BinarySearch<People> bs = new BinarySearch<>(people);
 
+            // select column and target, could we taken from user input if using scanner
+            // if using age, pass age as a string and it will be parsed to an integer in switch statement
             String columnToSearch = "name";
             String targetValue = "Joe";
 
+            // switch statement for columns 2 - 4 of people
             int result = switch (columnToSearch.toLowerCase()) {
                 case "name" -> bs.binarySearch(People::getName, targetValue);
                 case "surname" -> bs.binarySearch(People::getSurname, targetValue);
@@ -59,6 +79,7 @@ public class BinarySearch<T> {
                 default -> throw new IllegalArgumentException("Invalid column: " + columnToSearch);
             };
 
+            // if result found communicate to usrr else let them know its not found
             if (result != -1) {
                 System.out.println(targetValue + " was found in the " + columnToSearch + " list");
                 System.out.println("Record details: " + people[result]);
