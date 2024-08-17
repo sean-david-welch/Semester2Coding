dc29fbb35cb6c7ab68b720df9275b99b876032d1
seanwelch <seanwelch51199@icloud.com>
2024-08-17 11:44:17 +0100

    Food Product class

---
+++ TabaCode/src/question4/FoodProduct.java
@@ -0,0 +1,36 @@
+package question4;
+
+public class FoodProduct implements Comparable<FoodProduct> {
+    private final String name;
+    private final double price;
+    private final int healthyEatingIndex;
+
+    public FoodProduct(String name, double price, int healthyEatingIndex) {
+        this.name = name;
+        this.price = price;
+        this.healthyEatingIndex = healthyEatingIndex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getHealthyEatingIndex() {
+        return healthyEatingIndex;
+    }
+
+    @Override
+    public int compareTo(FoodProduct other) {
+        return Integer.compare(this.healthyEatingIndex, other.healthyEatingIndex);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (Price: $%.2f, Health Index: %d)", name, price, healthyEatingIndex);
+    }
+}
+
