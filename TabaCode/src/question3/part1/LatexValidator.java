1891cc72542ae9314735a879214eaa0d819d7d63
seanwelch <seanwelch51199@icloud.com>
2024-08-17 13:37:44 +0100

    comments latex

--- TabaCode/src/question3/part1/LatexValidator.java
+++ TabaCode/src/question3/part1/LatexValidator.java
@@ -8,23 +8,30 @@ import java.util.Objects;
 public class LatexValidator {
 
     public static void latexValidator(String filename) {
+        // init stack
         Stack stack = new Stack(100);
 
+        // read file
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             int lineNumber = 0;
+            // while in bounds
             while ((line = br.readLine()) != null) {
                 lineNumber++;
 
+                // push to stack if valid
                 if (line.contains("\\begin")) {
                     stack.push("\\begin");
                 }
 
+                // check for end
                 if (line.contains("\\end")) {
+                    // empty catch
                     if (stack.isEmpty()) {
                         System.out.println("Error: Unmatched \\end tag at line " + lineNumber);
                         return;
                     } else {
+                        // check top element for being
                         String topElement = (String) stack.peek();
                         if (topElement.equals("\\begin")) {
                             stack.pop();
@@ -47,9 +54,11 @@ public class LatexValidator {
     }
 
     public static void main(String[] args) {
+        // valid vile test
         String filename = Objects.requireNonNull(LatexValidator.class.getResource("/resources/latex.txt")).getPath();
         latexValidator(filename);
 
+        // invalid file tesrt
         String filename2 = Objects.requireNonNull(LatexValidator.class.getResource("/resources/invalidLatex.txt")).getPath();
         latexValidator(filename2);
     }
