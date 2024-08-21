6c05adf5795fe6c091c6ba1fc124eae376e48e41
seanwelch <seanwelch51199@icloud.com>
2024-08-21 19:55:13 +0100

    first two questions for question 3

---
+++ algorithmsCoding/AlgoTaba/src/main/java/taba/question3/ManageProducts.java
@@ -0,0 +1,47 @@
+package taba.question3;
+
+import taba.question2.Question2;
+
+import java.io.*;
+
+public class ManageProducts {
+    private static final System.Logger logger = System.getLogger(ManageProducts.class.getName());
+
+
+    public void readAndPrintFile(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            logger.log(System.Logger.Level.ERROR, "An error occurred!", e.getMessage());
+        }
+    }
+
+    public void writeMissingDepartmentToFile(String inputFilePath, String outputFilePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFilePath));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(outputFilePath))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+                String department = fields[3];
+
+                if (department == null || department.trim().isEmpty()) {
+                    bw.write(line);
+                    bw.newLine();
+                }
+            }
+        } catch (IOException e) {
+            logger.log(System.Logger.Level.ERROR, "An error occurred!", e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ManageProducts mp = new ManageProducts();
+        mp.readAndPrintFile("products.txt");
+        mp.writeMissingDepartmentToFile("products.txt", "MissingDepartment.txt");
+    }
+}
