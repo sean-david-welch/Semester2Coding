35de5b059fa2bfc29c4e9812196c4d84e06aa17d
seanwelch <seanwelch51199@icloud.com>
2024-08-21 18:05:17 +0100

    fix client code

--- algorithmsCoding/AlgoTaba/src/main/java/taba/ClientCode.java
+++ algorithmsCoding/AlgoTaba/src/main/java/taba/ClientCode.java
@@ -1,27 +1,22 @@
 package taba;
 
-import java.util.ArrayList;
+import java.util.List;
 
-/*
- * @author Hamilton1
- */
 public class ClientCode {
-
     public static void main(String[] args) {
-        // Use this code to generate the data in the ArrayList of drawers
-        ArrayList[] drawers = Taba2024.generateData();
+        // Use this code to generate the data in the List of drawers
+        List<List<Integer>> drawers = Taba2024.generateData();
 
-        // To access the individual drawers use the drawers[index]
+        // To access the individual drawers use the drawers.get(index)
         // The following code allows you to get the size of each drawer
-       
-        for (ArrayList drawer : drawers) {
+        for (List<Integer> drawer : drawers) {
             System.out.println(drawer.size());
         }
 
-        // To access the elements of a drawer use drawers[0][index]
-        for (int iCount = 0; iCount < drawers[0].size(); iCount++) {
-            //This prints the individual elements of drawer 0
-            System.out.println(drawers[0].get(iCount));
+        // To access the elements of a drawer use drawers.get(0).get(index)
+        for (int iCount = 0; iCount < drawers.getFirst().size(); iCount++) {
+            // This prints the individual elements of drawer 0
+            System.out.println(drawers.getFirst().get(iCount));
         }
     }
 }
