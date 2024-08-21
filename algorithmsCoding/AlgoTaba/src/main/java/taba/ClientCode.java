62d2f31acec2dfcc909bfc35c1394f4dde5e21f3
seanwelch <seanwelch51199@icloud.com>
2024-08-21 18:09:41 +0100

    remove silly class

--- algorithmsCoding/AlgoTaba/src/main/java/taba/ClientCode.java
+++ algorithmsCoding/AlgoTaba/src/main/java/taba/ClientCode.java
@@ -1,11 +1,26 @@
 package taba;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ClientCode {
+    static List<List<Integer>> drawers = new ArrayList<>(10);
+
+    public static List<List<Integer>> generateData() {
+        Random randomObject = new Random();
+        for (int iCount = 0; iCount < 10; iCount++) {
+            // Generates 10000 random integer numbers between -10,000 and 10,000
+            List<Integer> drawer = new ArrayList<>();
+            randomObject.ints(10000, -10000, 10000).forEach(drawer::add);
+            drawers.add(drawer);
+        }
+        return drawers;
+    }
+
     public static void main(String[] args) {
         // Use this code to generate the data in the List of drawers
-        List<List<Integer>> drawers = Taba2024.generateData();
+        List<List<Integer>> drawers = generateData();
 
         // To access the individual drawers use the drawers.get(index)
         // The following code allows you to get the size of each drawer
