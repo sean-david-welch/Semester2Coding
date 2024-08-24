fa7962ce423a1531f89fccd6235d912d802ab89d
seanwelch <seanwelch51199@icloud.com>
2024-08-24 11:04:17 +0100

    add dependancies

--- DsTaba/build.gradle.kts
+++ DsTaba/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.google.protobuf") version "0.9.4" // Protobuf plugin for Gradle
 }
 
 group = "taba"
@@ -10,10 +11,34 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.protobuf:protobuf-java:4.27.2")
+    implementation("io.grpc:grpc-netty-shaded:1.65.1")
+    implementation("io.grpc:grpc-protobuf:1.65.1")
+    implementation("io.grpc:grpc-stub:1.65.1")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+// Protobuf Plugin Configuration
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.1"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.65.1"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+            }
+        }
+    }
 }
